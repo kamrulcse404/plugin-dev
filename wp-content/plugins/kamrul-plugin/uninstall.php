fa4d25825a6d4a176be49ded49fc0874d7a53cdf
kamrulcse404 <kamrul.h01960@gmail.com>
2023-12-23 14:45:42 +0600

    when uninstall plgin delete plugin related custom post type relation taxonomoy

--- wp-content/plugins/kamrul-plugin/uninstall.php
+++ wp-content/plugins/kamrul-plugin/uninstall.php
@@ -13,8 +13,10 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 
 // clear database stored data 
-$books = get_post( array( 'post_type' => 'book', 'numberposts' => -1 ) );
+// access the database via sql  
+global $wpdb;
 
-foreach ($books as $book) {
-    wp_delete_post( $book->ID, true );
-}
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" );
+$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
+$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );
+
